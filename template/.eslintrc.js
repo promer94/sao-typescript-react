f7c6a13173e8509281e0b03b4958329682e06fea
Yixuan Xu <yixuanxu94@outlook.com>
2019-08-13 23:19:17 +0800

    ci: 🎡 update test snapshot to fix ci

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,13 +1,12 @@
 module.exports = {
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   extends: [
+    'xo-typescript',
     'xo-space',
     'xo-react/space',
-    'xo-typescript',
-    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    'plugin:prettier/recommended' // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'prettier/@typescript-eslint' // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
   ],
-  plugins: ['prettier', 'react-hooks'],
+  plugins: ['prettier'],
   parserOptions: {
     project: './tsconfig.json'
   },
