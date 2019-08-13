b2b9e85c9fb4bf850238f335f342b31550a0ded8
Yixuan Xu <yixuanxu94@outlook.com>
2019-08-14 00:18:55 +0800

    feat: ✨ update commit message to fit git-emoji
    Closes: #6

---
+++ changelog.config.js
@@ -0,0 +1,90 @@
+module.exports = {
+  disableEmoji: false,
+  list: [
+    'test',
+    'feat',
+    'fix',
+    'chore',
+    'docs',
+    'refactor',
+    'style',
+    'ci',
+    'perf',
+    'wip',
+    'merge'
+  ],
+  maxMessageLength: 64,
+  minMessageLength: 3,
+  questions: [
+    'type',
+    'scope',
+    'subject',
+    'body',
+    'breaking',
+    'issues',
+    'lerna'
+  ],
+  scopes: [],
+  types: {
+    chore: {
+      description: 'Build process or auxiliary tool changes',
+      emoji: '🚚',
+      value: 'chore'
+    },
+    ci: {
+      description: 'CI related changes',
+      emoji: '💚',
+      value: 'ci'
+    },
+    docs: {
+      description: 'Documentation only changes',
+      emoji: '📝',
+      value: 'docs'
+    },
+    feat: {
+      description: 'A new feature',
+      emoji: '✨',
+      value: 'feat'
+    },
+    fix: {
+      description: 'A bug fix',
+      emoji: '🐛',
+      value: 'fix'
+    },
+    perf: {
+      description: 'A code change that improves performance',
+      emoji: '⚡️',
+      value: 'perf'
+    },
+    refactor: {
+      description: 'A code change that neither fixes a bug or adds a feature',
+      emoji: '♻️',
+      value: 'refactor'
+    },
+    release: {
+      description: 'Create a release commit',
+      emoji: '🚀',
+      value: 'release'
+    },
+    style: {
+      description: 'Markup, white-space, formatting, missing semi-colons...',
+      emoji: '🎨',
+      value: 'style'
+    },
+    test: {
+      description: 'Adding missing tests',
+      emoji: '✅',
+      value: 'test'
+    },
+    wip: {
+      description: 'working in progress',
+      emoji: '🚧',
+      value: 'wip'
+    },
+    merge: {
+      description: 'merge branches',
+      emoji: '🔀',
+      value: 'merge'
+    }
+  }
+}
