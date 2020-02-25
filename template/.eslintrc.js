3467a6603c33b7afe5d0aae349dcbc35bde9e6f5
Yixuan Xu <yixuanxu94@outlook.com>
2020-02-25 18:22:44 +0800

    feat: ✨ 修改eslint 配置 添加path alias
    Closes: #3 #4

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,21 +1,30 @@
 module.exports = {
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   extends: [
-    'xo-typescript',
     'xo-space',
     'xo-react/space',
+    'plugin:prettier/recommended',
+    'prettier/react',
     'prettier/@typescript-eslint' // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
   ],
   plugins: ['prettier'],
-  parserOptions: {
-    project: './tsconfig.json'
-  },
   env: {
     browser: true,
     node: true,
     jest: true
   },
   rules: {
-    'react/jsx-tag-spacing': 'off'
+    'react/jsx-tag-spacing': 'off',
+    '@typescript-eslint/no-unnecessary-condition': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    'object-curly-spacing': ['error', 'always'],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    semi: ['error', 'never'],
+    'valid-jsdoc': 'off',
+    'react/prop-types': 'off',
+    'react/state-in-constructor': 'off',
+    '@typescript-eslint/restrict-template-expressions': 'off',
+    '@typescript-eslint/no-untyped-public-signature': 'off',
+    'no-unused-vars': 'off'
   }
 }
